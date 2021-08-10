5606991cdeae3623f5d02fedeeef548399c40c43
Gloria Edem <gloedem@gmail.com>
2021-08-10 20:23:54 +0000

    transfer form-submit logic to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,6 +32,21 @@ const data = [
 
 $(document).ready(function() {
   renderTweets(data);
+
+  const target = $('#target');
+  target.on('submit', function(event) {
+    event.preventDefault();
+    // console.log($(this));
+    const url = $(this).attr('action');
+    $.ajax({
+      method: 'POST',
+      url: url,
+      data: $(this).serialize(),
+      success: function(data) {
+        console.log(data);
+      },
+    });
+  });
 });
 
 const createTweetElement = (tweetObj) => {
