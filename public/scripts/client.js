41d28a105a58e5542016575e963c6e73e2337c1e
Gloria Edem <gloedem@gmail.com>
2021-08-10 20:35:08 +0000

    fetch tweets from server via AJAX GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461119232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 
 $(document).ready(function() {
-  renderTweets(data);
-
   const target = $('#target');
   target.on('submit', function(event) {
     event.preventDefault();
@@ -47,6 +20,18 @@ $(document).ready(function() {
       },
     });
   });
+
+  const loadTweets = function() {
+    $.ajax('/tweets/', {
+      method: 'GET',
+      success: function(response) {
+        console.log('response', response);
+        renderTweets(response);
+      }
+    });
+  };
+
+  loadTweets();
 });
 
 const createTweetElement = (tweetObj) => {
