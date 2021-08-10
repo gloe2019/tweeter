5694cd282b92da29c804486b00c3b28c3da41240
Gloria Edem <gloedem@gmail.com>
2021-08-10 20:17:24 +0000

    create AJAX POST request to send serialized form data to server

--- public/scripts/form-submit.js
+++ public/scripts/form-submit.js
@@ -1,10 +1,17 @@
 /* eslint-env jquery*/
 $(document).ready(() => {
   const target = $('#target');
-  target.on('submit', (event) => {
+  target.on('submit', function(event) {
     event.preventDefault();
-    
-    
+    // console.log($(this));
+    const url = $(this).attr('action');
+    $.ajax({
+      method: 'POST',
+      url: url,
+      data: $(this).serialize(),
+      success: function(data) {
+        console.log(data);
+      },
+    });
   });
-  
 });
