b494491b9adf8795440a1101c0ba239c8ec632fe
Gloria Edem <gloedem@gmail.com>
2021-08-10 18:10:49 +0000

    implement createTweetElement fnc to append new tweets to tweets container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,45 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
 
+$(document).ready(function() {
+  const createTweetElement = (tweetObj) => {
+    const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class='avi'>
+          <img src='${tweetObj.user.avatars}'></img>
+          <div class='username'>${tweetObj.user.name}</div>
+        </div>
+        <div class='handle'>${tweetObj.user.handle}</div>
+      </header>
+      <div class='tweet-body'>${tweetObj.content.text}</div>
+      <footer>
+        <div class='tweet-date'>${tweetObj['created_at']}</div>
+        <div class="tweet-icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+    return $tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
+
+});
